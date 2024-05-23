e15b9651c800e8e12af2f26965f631f6726383d0
yhoon3002 <yhoon3002@gmail.com>
2024-05-24 03:04:51 +0900

    solve Baekjoon No.1874 - 스택수열(python)

---
+++ problem/baekjoon/python/1874.py
@@ -0,0 +1,33 @@
+def stack():
+    _n = int(input())
+
+    _stack = []
+    _result = []
+    _temp = 1
+    _flag = False
+
+    for _ in range(_n):
+        _input = int(input())
+
+        for j in range(_temp, _n + 1):
+            if _input >= j:
+                _result.append("+")
+                _stack.append(j)
+                _temp += 1
+            else:
+                break
+
+        if _input == _stack[-1]:
+            _result.append("-")
+            _stack.pop()
+        else:
+            _flag = True
+
+    if _flag:
+        print("NO")
+    else:
+        for i in _result:
+            print(i)
+
+
+stack()
