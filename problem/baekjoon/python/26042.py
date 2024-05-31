f94483612dc7279e316a0c2b5a4203760332215b
yhoon3002 <yhoon3002@gmail.com>
2024-05-31 09:23:18 +0900

    solve Baekjoon No.27497 - 알파벳 블록(python) & No.26042 - 식당 입구 대기 줄(python)

---
+++ problem/baekjoon/python/26042.py
@@ -0,0 +1,68 @@
+import sys
+
+
+def line():
+    n = int(sys.stdin.readline())
+
+    # 현재의 줄 상태
+    queue = []
+    # 줄을 서서 대기하는 학생 수가 최대가 되었을 때의 학생 수
+    count = 0
+    # 줄을 서서 대기하는 학생 수가 최대가 되었을 때의 식당 입구의 맨 뒤에 대기 중인 학생의 번호
+    last = 0
+
+    for _ in range(n):
+        inputs = list(map(int, sys.stdin.readline().split()))
+
+        if len(inputs) == 1:
+            # 맨 앞쪽 사람 빼내기
+            queue.pop(0)
+        else:
+            # 줄 맨 뒤에 사람 세우기
+            queue.append(inputs[1])
+
+        if count < len(queue):
+            count = len(queue)
+            last = queue[-1]
+        elif count == len(queue):
+            if last > queue[-1]:
+                last = queue[-1]
+
+    print(count, last)
+
+
+line()
+
+
+# 맨 처음 안된 코드
+# def line():
+#     n = int(input())
+
+#     # 현재의 줄 상태
+#     queue = []
+#     # 줄을 서서 대기하는 학생 수가 최대가 되었을 때의 학생 수
+#     count = 0
+#     # 줄을 서서 대기하는 학생 수가 최대가 되었을 때의 식당 입구의 맨 뒤에 대기 중인 학생의 번호
+#     last = 0
+
+#     for _ in range(n):
+#         inputs = input()
+
+#         if len(inputs) == 1:
+#             # 맨 앞쪽 사람 빼내기
+#             queue.pop(0)
+#         else:
+#             # 줄 맨 뒤에 사람 세우기
+#             queue.append(inputs.split(" ")[1])
+
+#         if count < len(queue):
+#             count = len(queue)
+#             last = queue[-1]
+#         elif count == len(queue):
+#             if last > queue[-1]:
+#                 last = queue[-1]
+
+#     print(count, last)
+
+
+# line()
---
+++ problem/baekjoon/python/27497.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+
+def alphabet():
+    n = int(sys.stdin.readline())
+
+    order = []
+    deq = deque()
+
+    for _ in range(n):
+        inputs = sys.stdin.readline().split()
+
+        if int(inputs[0]) == 1:
+            deq.append(inputs[-1])
+            order.append("b")
+        elif int(inputs[0]) == 2:
+            deq.appendleft(inputs[-1])
+            order.append("f")
+        else:
+            if len(deq) > 0:
+                if order[-1] == "f":
+                    deq.popleft()
+                    order.pop()
+                else:
+                    deq.pop()
+                    order.pop()
+            else:
+                continue
+
+    if len(deq) == 0:
+        print(0)
+    else:
+        print("".join(deq))
+
+
+alphabet()
