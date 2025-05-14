6ef3a233f88c81855cb343ba4d51bef67471ed9d
0H00NI <yhoon3002@gmail.com>
2025-05-14 21:45:47 +0900

    solve Baekjoon No.9655 - 돌게임(Node.js)

--- problem/baekjoon/nodejs/9655.js
+++ problem/baekjoon/nodejs/9655.js
@@ -21,3 +21,40 @@ rl.on("line", function (line) {
 
     process.exit();
 });
+
+//
+//
+// DP로 푸는 방법
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = 0;
+let dp = [];
+
+rl.on("line", function (line) {
+    input = Number(line);
+
+    rl.close();
+}).on("close", function () {
+    dp = Array(input + 1).fill("");
+
+    dp[1] = "SK";
+    dp[2] = "CY";
+    dp[3] = "SK";
+
+    for (let i = 4; i <= input; i++) {
+        if (dp[i - 1] === "CY" || dp[i - 3] === "CY") {
+            dp[i] = "SK";
+        } else {
+            dp[i] = "CY";
+        }
+    }
+
+    console.log(dp[input]);
+
+    process.exit();
+});
