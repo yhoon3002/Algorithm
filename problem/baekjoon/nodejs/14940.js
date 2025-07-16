3f861d52f903b50cf57ec173738f8965fd836adc
0H00NI <yhoon3002@gmail.com>
2025-07-16 16:36:01 +0900

    solve Baekjoon No.14940 - 쉬운 최단 거리

--- problem/baekjoon/nodejs/14940.js
+++ problem/baekjoon/nodejs/14940.js
@@ -35,6 +35,36 @@ rl.on("line", function (line) {
     }
 
     // 3. BFS 탐색 시작
+    queue = [start];
+    answer[start[0]][start[1]] = 0;
+
+    dx = [-1, 1, 0, 0];
+    dy = [0, 0, -1, 1];
+
+    while (queue.length > 0) {
+        let [x, y] = queue.shift();
+
+        for (let i = 0; i < 4; i++) {
+            let nx = x + dx[i];
+            let ny = y + dy[i];
+
+            if (
+                nx >= 0 &&
+                nx < n &&
+                ny >= 0 &&
+                ny < m &&
+                mapArr[nx][ny] === 1 &&
+                answer[nx][ny] === -1
+            ) {
+                answer[nx][ny] = answer[x][y] + 1;
+                queue.push([nx, ny]);
+            }
+        }
+    }
+
+    answer = answer.map((el) => el.join(" "));
+
+    answer.map((el) => console.log(el));
 
     process.exit();
 });
